9df26344baf4ea38f94c7e809b087c761f7a4bc3
Anton Borisov <antobis1983@gmail.com>
2022-08-09 19:37:02 +0300

    modified brain-games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,7 +5,3 @@ def welcom_user():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
-
-
-if __name__ == '__main__':
-    welcom_user()
