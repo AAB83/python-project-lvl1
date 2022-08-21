3649e3f16f00c4927cabfff7595bbdcab34c484a
Anton Borisov <antobis1983@gmail.com>
2022-08-21 18:22:31 +0300

    modified prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 import random
-from brain_games.answer_user import reply
-from brain_games.games_api import compare_answer
-from brain_games.welcom_user import greet_user
+
+
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def calculate_answer(number):
@@ -13,21 +13,8 @@ def calculate_answer(number):
     return 'yes'
 
 
-def main_prime():
-    name_user = greet_user()
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    number_of_correct_answers = 0
-    while number_of_correct_answers < 3:
-        random_number = random.randint(1, 100)
-        answer_user = reply(random_number)
-        correct_answer = calculate_answer(random_number)
-        if compare_answer(answer_user, correct_answer):
-            print('Correct!')
-            number_of_correct_answers += 1
-        else:
-            print(f"""'{answer_user}' is wrong answer ;(. Correct answer was '{correct_answer}'.
-Let's try again, {name_user}!""")
-            break
-
-    if number_of_correct_answers == 3:
-        print(f'Congratulations, {name_user}!')
+def get_question_and_answer():
+    random_number = random.randint(1, 100)
+    correct_answer = calculate_answer(random_number)
+    question = str(random_number)
+    return question, correct_answer
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games.games.prime import main_prime
+from brain_games.engine import run_game
+from brain_games.games import prime
 
 
 def main():
-    main_prime()
+    run_game(prime)
 
 
 if __name__ == '__main__':
