3055d1174c3801262d79d849a4f61962f4c9ac3c
Anton Borisov <antobis1983@gmail.com>
2022-08-21 20:11:39 +0300

    modified progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,41 +1,28 @@
 import random
-from brain_games.answer_user import reply
-from brain_games.games_api import compare_answer
-from brain_games.welcom_user import greet_user
+
+
+DESCRIPTION = 'What number is missing in the progression?'
 
 
 def calculate_answer(index_correct_answer, random_number_one, step_progression):
     return str(random_number_one + step_progression * index_correct_answer)
 
 
-def main_progression():
-    name_user = greet_user()
-    print('What number is missing in the progression?')
-    number_of_correct_answers = 0
-    while number_of_correct_answers < 3:
-        random_number_one = random.randint(1, 20)
-        step_progression = random.randint(2, 10)
-        index_correct_answer = random.randint(1, 9)
-        i = 1
-        progression = str(random_number_one)
-        while i < 10:
-            if i != index_correct_answer:
-                progression = progression + ' ' + \
-                    str(random_number_one + step_progression * i)
-            else:
-                progression = progression + ' ' + '..'
-            i += 1
-        answer_user = reply(progression)
-        correct_answer = calculate_answer(index_correct_answer,
-                                          random_number_one, step_progression)
-        print(correct_answer)
-        if compare_answer(answer_user, correct_answer):
-            print('Correct!')
-            number_of_correct_answers += 1
+def get_question_and_answer():
+    random_number_one = random.randint(1, 20)
+    step_progression = random.randint(2, 10)
+    index_correct_answer = random.randint(1, 9)
+    i = 1
+    progression = str(random_number_one)
+    while i < 10:
+        if i != index_correct_answer:
+            progression = progression + ' ' + \
+                str(random_number_one + step_progression * i)
         else:
-            print(f"""'{answer_user}' is wrong answer ;(. Correct answer was '{correct_answer}'.
-Let's try again, {name_user}!""")
-            break
+            progression = progression + ' ' + '..'
+        i += 1
 
-    if number_of_correct_answers == 3:
-        print(f'Congratulations, {name_user}!')
+    question = progression
+    correct_answer = calculate_answer(index_correct_answer,
+                                      random_number_one, step_progression)
+    return question, correct_answer
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games.games.progression import main_progression
+from brain_games.engine import run_game
+from brain_games.games import progression
 
 
 def main():
-    main_progression()
+    run_game(progression)
 
 
 if __name__ == '__main__':
