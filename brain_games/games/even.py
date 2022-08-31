7f7d00e90a1419d567606c0d17af49abadff06df
Anton Borisov <antobis1983@gmail.com>
2022-08-31 22:55:51 +0300

    modified even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,15 +4,7 @@ import random
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def calculate_answer(number):
-    if number % 2 == 0:
-        return 'yes'
-    else:
-        return 'no'
-
-
 def get_question_and_answer():
     random_number = random.randint(1, 100)
     question = random_number
-    correct_answer = calculate_answer(random_number)
-    return question, correct_answer
+    return question, 'yes' if random_number % 2 == 0 else 'no'
