4c0f6dcb0f390121ba17b25c1c979a66a291dfbe
Anton Borisov <antobis1983@gmail.com>
2022-08-31 22:42:11 +0300

    modified calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,24 +1,19 @@
 import random
+import operator
 
 
 DESCRIPTION = 'What is the result of the expression?'
 
 
-def calculate_answer(number_one, number_two, action):
-    if action == '+':
-        return str(number_one + number_two)
-    elif action == '-':
-        return str(number_one - number_two)
-    elif action == '*':
-        return str(number_one * number_two)
-
-
 def get_question_and_answer():
     random_number_one = random.randint(1, 100)
     random_number_two = random.randint(1, 100)
-    actions = ('+', '-', '*')
-    operation = actions[random.randint(0, 2)]
+    operations = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+    }
+    operation = random.choice(list(operations.keys()))
     question = f"{str(random_number_one)} {operation} {str(random_number_two)}"
-    correct_answer = calculate_answer(random_number_one,
-                                      random_number_two, operation)
+    correct_answer = str(operations[operation](random_number_one, random_number_two))
     return question, correct_answer
