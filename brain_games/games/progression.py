3396a15f7025e4df787490e02fe7516d127e8e03
Anton Borisov <antobis1983@gmail.com>
2022-09-02 00:48:36 +0300

    modified progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,18 +7,14 @@ DESCRIPTION = 'What number is missing in the progression?'
 def get_question_and_answer():
     random_number_one = random.randint(1, 20)
     step_progression = random.randint(2, 10)
-    index_correct_answer = random.randint(1, 9)
-    i = 1
-    progression = str(random_number_one)
-    while i < 10:
-        if i != index_correct_answer:
-            progression = f"{progression} " \
-                          f"{str(random_number_one + step_progression * i)}"
-        else:
-            progression = f"{progression} .."
-        i += 1
-
-    question = progression
+    progression_length = 10
+    progression_end = random_number_one + step_progression * progression_length
+    index_correct_answer = random.randint(1, progression_length - 1)
+    progression = list(range(random_number_one, progression_end, step_progression))
+    progression[index_correct_answer] = '..'
+    question = ''
+    for i in progression:
+        question = f'{question} {i}'
     correct_answer = \
         str(random_number_one + step_progression * index_correct_answer)
     return question, correct_answer
