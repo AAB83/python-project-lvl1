81191e3bd9fc918ab03971828467cb86d2182c5d
Anton Borisov <antobis1983@gmail.com>
2022-09-01 23:47:15 +0300

    modified prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,20 +1,11 @@
 import random
 
+from brain_games.is_prime import is_prime
 
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def calculate_answer(number):
-    if number == 1:
-        return 'no'
-    for i in range(2, number // 2 + 1):
-        if number % i == 0:
-            return 'no'
-    return 'yes'
-
-
 def get_question_and_answer():
     random_number = random.randint(1, 100)
-    correct_answer = calculate_answer(random_number)
     question = str(random_number)
-    return question, correct_answer
+    return question, 'yes' if is_prime(random_number) else 'no'
