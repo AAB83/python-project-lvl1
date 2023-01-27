53afbb9d272fb02a5adc848957dc8062dd267800
Anton Borisov <antobis1983@gmail.com>
2023-01-28 01:57:32 +0300

    update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -9,13 +9,13 @@ def get_question_and_answer():
     step = random.randint(2, 10)
     length = 10
     end = start + step * length
-    index_correct_answer = random.randint(0, length - 1)
+    index_correct_answer = random.randint(1, length - 1)
     progression = \
         list(range(start, end, step))
     progression[index_correct_answer] = '..'
     question = str(start)
-    for i in progression:
-        question = f'{question} {i}'
+    for i in range(1, length):
+        question = f'{question} {progression[i]}'
     correct_answer = \
         str(start + step * index_correct_answer)
     return question, correct_answer
