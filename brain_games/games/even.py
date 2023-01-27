6a6b9e6487256fff99548b228f684df60db6eec2
Anton Borisov <antobis1983@gmail.com>
2023-01-28 01:19:11 +0300

    uodate even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,6 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def get_question_and_answer():
-    random_number = random.randint(1, 100)
-    question = random_number
-    correct_answer = 'yes' if random_number % 2 == 0 else 'no'
+    question = random.randint(1, 100)
+    correct_answer = 'yes' if question % 2 == 0 else 'no'
     return question, correct_answer
