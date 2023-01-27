71a0a839731f9053fecadb7960585426f9cc0a7a
Anton Borisov <antobis1983@gmail.com>
2023-01-28 01:29:22 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,4 +8,5 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def get_question_and_answer():
     random_number = random.randint(1, 100)
     question = str(random_number)
-    return question, 'yes' if is_prime(random_number) else 'no'
+    correct_answer = 'yes' if is_prime(random_number) else 'no'
+    return question, correct_answer
